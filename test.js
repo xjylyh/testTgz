438771ee37f3fd32907b8f39df2e2b0ae1aa3547
xiejiyao_dxm <xiejiyao@duxiaoman.com>
2018-10-23 17:03:02 +0800

    store changes

--- test.js
+++ test.js
@@ -2,22 +2,6 @@ const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const scheduleJob = require('node-schedule').scheduleJob;
-// async function scheduleCronstyle(){
-//     try{
-//         let targetPwd = path.resolve('/home/work/.config/verdaccio');
-//         let {stderr,stdout} = await exec(`cd ${targetPwd}`);
-//         console.log(stdout);
-//         let nextMode = await scheduleJob('2 * * * * *');
-//         nextMode.then(()=>{
-//             console.log('scheduleCronstyle'+new Date());
-//             let {stderr,stdout} = await exec('git status');
-//             console.log(stdout);
-//         })
-//     }catch(e){
-
-//     }
-// }
-// scheduleCronstyle();
 
 scheduleJob('2 * * * * *',async ()=>{
     try{
