27553554308d27d8c1a37a1fc5250858d9a255ea
xiejiyao_dxm <xiejiyao@duxiaoman.com>
2018-10-23 15:30:15 +0800

    store changes

--- test.js
+++ test.js
@@ -21,8 +21,14 @@ const scheduleJob = util.promisify(require('node-schedule').scheduleJob);
 
 async function a(){
     try{
-        let {error,stderr,stdout} = await exec('git status');
-        console.log(error,stderr,stdout);
+        let {stderr,stdout} = await exec('git status');
+        console.log(stdout);
+        if(!stdout.toString().includes('nothing to commit, working tree clean')){
+            await exec('git add .');
+            await exec('git commit -m "store changes" ');
+            let {stderr,stdout} = await exec('git push origin master');
+            console.log(stdout);
+        }
     }catch(err){
         if(err){
             console.log('catch: '+err);
